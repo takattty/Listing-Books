58c6017bd8aacad4869123cfe21e1f5817f73d0f
takattty <r9r_celtics_t9t@icloud.com>
2019-06-26 01:08:45 +0900

    change-anyfiles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express-app' });
+  res.render('index', { title: 'Express-app', content: 'Express-page' });
 });
 
 module.exports = router;
