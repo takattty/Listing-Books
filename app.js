55d84a3195b7dc7beb5582b0716c02892393a6f4
takattty <r9r_celtics_t9t@icloud.com>
2019-06-26 01:14:59 +0900

    change-ver-to-let

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index').default;
+let usersRouter = require('./routes/users');
 
 console.log('Server start');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
