79a8d5fe5e972df00c522ca94fae5a15e2272f37
takattty <r9r_celtics_t9t@icloud.com>
2019-06-28 21:28:29 +0900

    change-app.js files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
 
 const app = express();
 console.log('Server start!');
@@ -20,8 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
