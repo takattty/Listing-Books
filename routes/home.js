a7fa857b64969a889e7b94a7adaec5086943becd
takattty <r9r_celtics_t9t@icloud.com>
2019-06-28 21:56:26 +0900

    change home.js files

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { home_title: 'Sharechat!' });
+  res.render('home', { home_title: 'Sharechat' });
 });
 
 module.exports = router;
