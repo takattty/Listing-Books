5c158d302cd223228560d9fe75f45dffe95ca489
takattty <r9r_celtics_t9t@icloud.com>
2019-06-28 21:25:41 +0900

    change-anyfiles

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { home_title: 'Sharechat!' });
+});
+
+module.exports = router;
