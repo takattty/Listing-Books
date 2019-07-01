ef7ba4c91077383eb9dabc2b5d9f42459272391f
takattty <r9r_celtics_t9t@icloud.com>
2019-07-01 19:59:49 +0900

    change account.js file

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/createaccount', function(req, res, next) {
+router.get('/account/create', function(req, res, next) {
     res.render('account', {});
+});
+
+router.get('/account/id/edit', function(req, res, next) {
+    res.render('account_edit', {});
 });
 
 module.exports = router;
