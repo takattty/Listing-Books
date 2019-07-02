02a96b6e2d16371bbcac335e31f953742fae6ed6
takattty <r9r_celtics_t9t@icloud.com>
2019-07-02 18:15:40 +0900

    change account.css file

--- routes/account.js
+++ routes/account.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/signup', function(req, res, next) {
     const signup_text = {
-        title: 'ここではアカウントの新規作成が出来ます。名前、パスワード、自己紹介を書いてアカウントを作成しよう！'
+        title1: 'ここではアカウントの新規作成が出来ます。',
+        title2: '名前、パスワード、自己紹介を書いてアカウントを作成しよう！'
     };
     res.render('account', signup_text);
 });
