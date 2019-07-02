27b02321afebd69c675f11b78185d23eea815f0d
takattty <r9r_celtics_t9t@icloud.com>
2019-07-02 20:32:59 +0900

    change account.ejd&.js files

--- routes/account.js
+++ routes/account.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get('/signup', function(req, res, next) {
     const signup_text = {
         title1: 'ここではアカウントの新規作成が出来ます。',
-        title2: '名前、パスワード、自己紹介を書いてアカウントを作成しよう！'
+        title2: '名前、パスワード、自己紹介を書いてアカウントを作成しよう！',
+        create_bottom: 'アカウントを作成する' 
     };
     res.render('account', signup_text);
 });
