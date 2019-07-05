cfa51194bf133029fdd283e352bda2add27ef1ea
takattty <r9r_celtics_t9t@icloud.com>
2019-07-05 10:19:27 +0900

    change room_index.ejs file

--- routes/room_index.js
+++ routes/room_index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/index', function(req, res, next) { //router.getメソッドで'/'にアクセスした際の表示を行う。
+router.get('/index', function(req, res, next) { //今回はここでDatabaseからRoom情報を取得して、ejsファイルに表示する処理を書く。
   res.render('room_index');
 });
 
