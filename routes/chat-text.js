390ad2e8c6a9854f8c6ddb2fe6a10f72b976e8b9
takattty <r9r_celtics_t9t@icloud.com>
2019-07-06 13:06:40 +0900

    create chat-text files

--- routes/chat-text.js
+++ routes/chat-text.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    const   {
-
+    const chatTextShow = {
+        delete_bottom: '削除',
+        edit_bottom: '編集'
     };
-    res.render('chat-text', );
+    res.render('chat-text', chatTextShow);
 });
 
 router.get('/edit', function(req, res, next) {
-    const   {
-
+    const chatTextEdit = {
+        delete_bottom: '削除',
+        edit_bottom: '更新'
     }
-    res.render('chat-text', );
+    res.render('chat-text', chatTextEdit );
 });
 
 module.exports = router;
