36bcfd9b08c5c9c64eff52bdd978036865a7dea1
takattty <r9r_celtics_t9t@icloud.com>
2019-07-06 17:40:01 +0900

    change profile-responsive.css file

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/id', function(req, res, next) {
-  res.render('profile', { bottom: '編集' });
+  res.render('profile', { bottom: '編集する' });
 });
 
 module.exports = router;
