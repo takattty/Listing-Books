5a99daff77568d42e69d84115b82be70c59ee68b
takattty <r9r_celtics_t9t@icloud.com>
2019-07-06 16:44:58 +0900

    change any files+create profile page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const roomIndexRouter = require('./routes/room-index');
 const roomLoginRouter = require('./routes/room-login');
 const roomCreateRouter = require('./routes/room-create');
 const chatTextRouter = require('./routes/chat-text');
-const accountEditRouter = require('./routes/account-edit');
+const profileRouter = require('./routes/profile');
 
 const app = express();
 console.log('Server start!');
@@ -39,7 +39,7 @@ app.use('/room', roomIndexRouter);
 app.use('/room', roomLoginRouter);
 app.use('/room', roomCreateRouter);
 app.use('/chat/text/id', chatTextRouter);
-app.use('/account', accountEditRouter);
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/edit', function(req, res, next) {
-  res.render('account-edit');
+router.get('/id', function(req, res, next) {
+  res.render('profile', { bottom: '設定' });
 });
 
 module.exports = router;
