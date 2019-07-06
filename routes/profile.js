c7d82fbd07b0c8b102e45b587cbe698a42f70b5f
takattty <r9r_celtics_t9t@icloud.com>
2019-07-06 17:13:03 +0900

    change buttom

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/id', function(req, res, next) {
-  res.render('profile', { bottom: '設定' });
+  res.render('profile', { bottom: '編集' });
 });
 
 module.exports = router;
