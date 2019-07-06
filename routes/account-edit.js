ac3f917e6f5950d528083b5d87c5eb3cd8924688
takattty <r9r_celtics_t9t@icloud.com>
2019-07-06 15:32:19 +0900

    create account-edit.js file

--- routes/account-edit.js
+++ routes/account-edit.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/edit', function(req, res, next) { //router.getメソッドで'/'にアクセスした際の表示を行う。
+router.get('/edit', function(req, res, next) {
   res.render('account-edit');
 });
 
