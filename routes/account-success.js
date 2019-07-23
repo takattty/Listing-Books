950b4aefd9eb02372eee328cc3c4331077c2d3cc
takattty <r9r_celtics_t9t@icloud.com>
2019-07-23 11:35:23 +0900

    change account-success.js

--- routes/account-success.js
+++ routes/account-success.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../mysqlConnection');
 
 /* GET home page. */
 router.get('/success', function(req, res, next) { //router.getメソッドで'/'にアクセスした際の表示を行う。
-  res.render('account-success', { user_name: 'ユーザー名が入ります。'});
+  var query = 'SELECT * FROM sharechat_test.account';
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err;
+    console.log(rows);
+    res.render('account-success', { user_name: rows[0].name});
+  });
 });
 
 module.exports = router;
