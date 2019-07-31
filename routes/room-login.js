5d053bf569727e76abe91f8793c77ca8618f6e97
takattty <r9r_celtics_t9t@icloud.com>
2019-07-31 17:15:15 +0900

    change room-index & login files

--- routes/room-login.js
+++ routes/room-login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/id/login', function(req, res, next) { 
-  res.render('room-login');
+	res.render('room-login');
 });
 
 module.exports = router;
