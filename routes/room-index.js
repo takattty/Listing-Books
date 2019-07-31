f1f52a4c9ad3a1fd0895b03340a7e56cec14aaad
takattty <r9r_celtics_t9t@icloud.com>
2019-07-31 14:41:28 +0900

    change room-index page

--- routes/room-index.js
+++ routes/room-index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../mysqlConnection');
 
 /* GET home page. */
-router.get('/index', function(req, res, next) { //今回はここでDatabaseからRoom情報を取得して、ejsファイルに表示する処理を書く。
-  res.render('room-index');
+router.get('/index', function(req, res, next) {
+  let query = 'SELECT room_id, room_name FROM room';
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+    console.log(rows);
+    res.render('room-index', { room_info: rows });
+  });
 });
 
 module.exports = router;
