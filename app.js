d3fa71f2f3342df90e42e9232ecd9bb4172cba07
takattty <r9r_celtics_t9t@icloud.com>
2019-08-01 13:16:22 +0900

    change app.js file

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: 'sharechat-secret',
-  resave: false,
-  saveUninitialized: false
+	secret: 'sharechat-secret',
+	resave: false,
+	saveUninitialized: false
 }));
 
 app.use(logger('dev'));
@@ -52,18 +52,18 @@ app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};	
+	
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
