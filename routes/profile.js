4189c228cba00cb977c5d1aadd2f3f12f935f131
takattty <r9r_celtics_t9t@icloud.com>
2019-08-01 15:58:01 +0900

    change profile files

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../mysqlConnection');
 
 /* GET home page. */
-router.get('/id', function(req, res, next) {
-  res.render('profile', { bottom: '編集する' });
+router.get('/:id', function(req, res, next) {
+	let user_id = req.params.id;
+	let query = 'SELECT id, name, si FROM account WHERE id =' + user_id;
+	connection.query(query, (err, rows) => {
+		if (err) throw err;
+		console.log(rows[0]);
+		res.render('profile', { 
+			user_name: rows[0].name, 
+			user_si: rows[0].si,
+			user_id: rows[0].id });
+		});
 });
 
 module.exports = router;
