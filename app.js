2f01bbd154fd8c4732d7c7ac9d631d171189d826
takattty <r9r_celtics_t9t@icloud.com>
2019-12-10 23:51:33 +0900

    changed the files

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const createError = require('http-errors');
 const express = require('express');
+const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const debug = require('debug')('sharechat:server');
+const http = require('http');
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);//この子が探しているサーバーの待ち状態。
+server.on('error', onError);
+server.on('listening', onListening);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,8 +28,15 @@ const roomCreateRouter = require('./routes/room-create');
 const profileRouter = require('./routes/profile');
 const chatRouter = require('./routes/chat');
 
-const app = express();
 console.log('Server start!');
+//Socket.io
+const io = require('socket.io').listen(server);
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log('サーバでの処理=' + msg);
+      io.emit('message', msg);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -66,4 +83,64 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-module.exports = app;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	const port = parseInt(val, 10);
+  
+	if (isNaN(port)) {
+	  // named pipe
+	  return val;
+	}
+  
+	if (port >= 0) {
+	  // port number
+	  return port;
+	}
+  
+	return false;
+}
+  
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  
+function onError(error) {
+	if (error.syscall !== 'listen') {
+	  throw error;
+	}
+  
+	const bind = typeof port === 'string'
+	  ? 'Pipe ' + port
+	  : 'Port ' + port;
+  
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+	  case 'EACCES':
+		console.error(bind + ' requires elevated privileges');
+		process.exit(1);
+		break;
+	  case 'EADDRINUSE':
+		console.error(bind + ' is already in use');
+		process.exit(1);
+		break;
+	  default:
+		throw error;
+	}
+}
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+function onListening() {
+	const addr = server.address();
+	const bind = typeof addr === 'string'
+	  ? 'pipe ' + addr
+	  : 'port ' + addr.port;
+	debug('Listening on ' + bind);
+}  
+
+module.exports = io;
