d5db9c60e72724f42c088ddbef34a2c5c4ebe7a1
takattty <r9r_celtics_t9t@icloud.com>
2019-12-27 20:56:46 +0900

    clearn tab space

--- app.js
+++ app.js
@@ -148,57 +148,57 @@ app.use(function(err, req, res, next) {
 
 function normalizePort(val) {
 	const port = parseInt(val, 10);
-  
+	
 	if (isNaN(port)) {
-	  // named pipe
-	  return val;
+		// named pipe
+		return val;
 	}
-  
+	
 	if (port >= 0) {
-	  // port number
-	  return port;
+		// port number
+		return port;
 	}
-  
+	
 	return false;
 }
-  
-  /**
-   * Event listener for HTTP server "error" event.
-   */
-  
+	
+	/**
+	 * Event listener for HTTP server "error" event.
+	 */
+	
 function onError(error) {
 	if (error.syscall !== 'listen') {
-	  throw error;
+		throw error;
 	}
-  
+	
 	const bind = typeof port === 'string'
-	  ? 'Pipe ' + port
-	  : 'Port ' + port;
-  
+		? 'Pipe ' + port
+		: 'Port ' + port;
+	
 	// handle specific listen errors with friendly messages
 	switch (error.code) {
-	  case 'EACCES':
+		case 'EACCES':
 		console.error(bind + ' requires elevated privileges');
 		process.exit(1);
 		break;
-	  case 'EADDRINUSE':
+		case 'EADDRINUSE':
 		console.error(bind + ' is already in use');
 		process.exit(1);
 		break;
-	  default:
+		default:
 		throw error;
 	}
 }
-  
-  /**
-   * Event listener for HTTP server "listening" event.
-   */
-  
+	
+	/**
+	 * Event listener for HTTP server "listening" event.
+	 */
+	
 function onListening() {
 	const addr = server.address();
 	const bind = typeof addr === 'string'
-	  ? 'pipe ' + addr
-	  : 'port ' + addr.port;
+		? 'pipe ' + addr
+		: 'port ' + addr.port;
 	debug('Listening on ' + bind);
 }  
 
--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../mysqlConnection');
-const moment = require('moment');
 
 // router.get('/:id', function(req, res) {
 //     let room_id = req.params.id;//roomの識別
@@ -58,101 +57,99 @@ const moment = require('moment');
 
 //各ルームのコメント詳細ページ
 router.get('/:room_id/text/:text_id/account/:user_id', function(req, res, next) {
-    let text_id = req.params.text_id;
-    let user_id = req.params.user_id;
-    connection.beginTransaction((err) => {
-        if (err) { throw err; }
-        connection.query('SELECT * FROM message WHERE message_id=' + text_id, (err, row1) => {
-            if (err) {
-                console.log('最初でミスってる！');
-            }
-            console.log(row1);
-            console.log(row1[0].text);
-            console.log(row1[0].room_id);
-            connection.query('SELECT name FROM account WHERE id=?', user_id, (err, row2) => {
-                if (err) {
-                    console.log('2つ目のクエリでミスってる！');
-                }
-                console.log(row2);
-                console.log(row2[0].name);
-                connection.commit((err) => {
-                    if (err) {
-                        console.log('最後のコミットでミスってる！');
-                    }
-                    console.log('success!!!!!');
-                    const chatTextShow = {
-                        edit_bottom: '編集',
-                        user: row2[0],
-                        text: row1[0]
-                    };
-                    res.render('chat-text', chatTextShow);
-                });
-            });
-        });
-    });
+	let text_id = req.params.text_id;
+	let user_id = req.params.user_id;
+	connection.beginTransaction((err) => {
+		if (err) { throw err; }
+		connection.query('SELECT * FROM message WHERE message_id=' + text_id, (err, row1) => {
+			if (err) {
+					console.log('最初でミスってる！');
+			}
+			console.log(row1);
+			console.log(row1[0].text);
+			console.log(row1[0].room_id);
+			connection.query('SELECT name FROM account WHERE id=?', user_id, (err, row2) => {
+				if (err) {
+					console.log('2つ目のクエリでミスってる！');
+				}
+				console.log(row2);
+				console.log(row2[0].name);
+				connection.commit((err) => {
+					if (err) {
+						console.log('最後のコミットでミスってる！');
+					}
+					console.log('success!!!!!');
+					const chatTextShow = {
+						edit_bottom: '編集',
+						user: row2[0],
+						text: row1[0]
+					};
+					res.render('chat-text', chatTextShow);
+				});
+			});
+		});
+	});
 });
 
 //各ルームのコメントの編集ページ
 router.get('/:room_id/text/:text_id/account/:user_id/edit', function(req, res, next) {
-    let text_id = req.params.text_id;
-    let user_id = req.params.user_id;
-    connection.beginTransaction((err) => {
-        if (err) { throw err; }
-        connection.query('SELECT * FROM message WHERE message_id=' + text_id, (err, row1) => {
-            if (err) {
-                console.log('最初でミスってる！');
-            }
-            console.log(row1);
-            console.log(row1[0].text);
-            console.log(row1[0].room_id);
-            connection.query('SELECT name FROM account WHERE id=?', user_id, (err, row2) => {
-                if (err) {
-                    console.log('2つ目のクエリでミスってる！');
-                }
-                console.log(row2);
-                console.log(row2[0].name);
-                connection.commit((err) => {
-                    if (err) {
-                        console.log('最後のコミットでミスってる！');
-                    }
-                    console.log('success!!!!!');
-                    const chatTextEdit = {
-                        delete_bottom: '削除',
-                        edit_bottom: '更新',
-                        user: row2[0],
-                        text: row1[0]
-                    }
-                    res.render('chat-text-edit', chatTextEdit );
-                })
-            });
-        });
-    });
+	let text_id = req.params.text_id;
+	let user_id = req.params.user_id;
+	connection.beginTransaction((err) => {
+		if (err) { throw err; }
+		connection.query('SELECT * FROM message WHERE message_id=' + text_id, (err, row1) => {
+			if (err) {
+					console.log('最初でミスってる！');
+			}
+			console.log(row1);
+			console.log(row1[0].text);
+			console.log(row1[0].room_id);
+			connection.query('SELECT name FROM account WHERE id=?', user_id, (err, row2) => {
+				if (err) {
+						console.log('2つ目のクエリでミスってる！');
+				}
+				console.log(row2);
+				console.log(row2[0].name);
+				connection.commit((err) => {
+					if (err) {
+						console.log('最後のコミットでミスってる！');
+					}
+					console.log('success!!!!!');
+					const chatTextEdit = {
+						delete_bottom: '削除',
+						edit_bottom: '更新',
+						user: row2[0],
+						text: row1[0]
+					}
+					res.render('chat-text-edit', chatTextEdit );
+				});
+			});
+		});
+	});
 });
 
 router.post('/:room_id/text/:text_id/account/:user_id/edit', (req, res, next) => {
-    let room_id = req.params.room_id;
-    let text_id = req.params.text_id;
-    let text = req.body.text;
-    let num = req.body.kind;
-    console.log(text);
-    console.log(num);
-
-    if (num == 1) {
-        console.log('削除ボタンの処理が出来てるよ！');
-        let query = 'DELETE FROM message WHERE message_id =' + text_id;
-        connection.query(query, (err, rows) => {
-            if (err) throw err;
-            res.redirect('/chat/' + room_id);
-        });
-    } 
-    if (num == 2) {
-        console.log('更新の処理が出来るよ！');
-        connection.query('UPDATE message SET text=? WHERE message_id=?', [text, text_id], (err, rows) => {
-            if (err) throw err;
-            res.redirect('/chat/' + req.params.room_id + '/text/' + req.params.text_id + '/account/' + req.params.user_id);
-        });
-    }
-
+	let room_id = req.params.room_id;
+	let text_id = req.params.text_id;
+	let text = req.body.text;
+	let num = req.body.kind;
+	console.log(text);
+	console.log(num);
+	if (num == 1) {
+		console.log('削除ボタンの処理が出来てるよ！');
+		let query = 'DELETE FROM message WHERE message_id =' + text_id;
+		connection.query(query, (err, rows) => {
+			if (err) throw err;
+			res.redirect('/chat/' + room_id);
+		});
+	} 
+	if (num == 2) {
+		console.log('更新の処理が出来るよ！');
+		connection.query('UPDATE message SET text=? WHERE message_id=?', [text, text_id], (err, rows) => {
+			if (err) throw err;
+			res.redirect('/chat/' + req.params.room_id + '/text/' + req.params.text_id + '/account/' + req.params.user_id);
+		});
+	}
 });
 
 module.exports = router;
