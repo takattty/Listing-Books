fc88912b24f598d3e03f074660cf23d06130c508
takattty <r9r_celtics_t9t@icloud.com>
2020-06-16 21:03:37 +0900

    added unescape function in room-index file

--- routes/room-index.js
+++ routes/room-index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../mysqlConnection');
+const unescape = require('../public/javascripts/escape/unescape');
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
   const query = 'SELECT room_id, room_name FROM room';
   connection.query(query, (err, rows) => {
     if (err) throw err;
+    const unescaped_room_name = unescape(rows[0].room_name);
+    rows[0].room_name = unescaped_room_name;
     res.render('room-index', { room_info: rows });
   });
 });
